d54611479297b260ea136d047f62296b38656faf
Zahirul Islam <45603482+zahlambo@users.noreply.github.com>
2022-01-20 20:47:40 +0600

    Update 8_puzzle_3x3_grid__astar.py

--- 8_puzzle_3x3_grid__astar.py
+++ 8_puzzle_3x3_grid__astar.py
@@ -1,13 +1,13 @@
 from copy import deepcopy
 from colorama import Fore, Back, Style
 
-#direction matrix
+# direction matrix
 DIRECTIONS = {"U": [-1, 0], "D": [1, 0], "L": [0, -1], "R": [0, 1]}
-#target matrix
-END = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+# target matrix
 
 
 # unicode for draw puzzle in command promt or terminal
+
 left_down_angle = '\u2514'
 right_down_angle = '\u2518'
 right_up_angle = '\u2510'
@@ -19,16 +19,24 @@ bottom_junction = '\u2534'
 right_junction = '\u2524'
 left_junction = '\u251C'
 
-#bar color
+# bar color
 bar = Style.BRIGHT + Fore.CYAN + '\u2502' + Fore.RESET + Style.RESET_ALL
 dash = '\u2500'
 
-#Line draw code
-first_line = Style.BRIGHT + Fore.CYAN + left_up_angle + dash + dash + dash + top_junction + dash + dash + dash + top_junction + dash + dash + dash + right_up_angle + Fore.RESET + Style.RESET_ALL
-middle_line = Style.BRIGHT + Fore.CYAN + left_junction + dash + dash + dash + middle_junction + dash + dash + dash + middle_junction + dash + dash + dash + right_junction + Fore.RESET + Style.RESET_ALL
-last_line = Style.BRIGHT + Fore.CYAN + left_down_angle + dash + dash + dash + bottom_junction + dash + dash + dash + bottom_junction + dash + dash + dash + right_down_angle + Fore.RESET + Style.RESET_ALL
+# Line draw code
+first_line = Style.BRIGHT + Fore.CYAN + left_up_angle + dash + dash + dash + top_junction + dash + \
+    dash + dash + top_junction + dash + dash + dash + \
+    right_up_angle + Fore.RESET + Style.RESET_ALL
+middle_line = Style.BRIGHT + Fore.CYAN + left_junction + dash + dash + dash + middle_junction + dash + \
+    dash + dash + middle_junction + dash + dash + dash + \
+    right_junction + Fore.RESET + Style.RESET_ALL
+last_line = Style.BRIGHT + Fore.CYAN + left_down_angle + dash + dash + dash + bottom_junction + dash + \
+    dash + dash + bottom_junction + dash + dash + dash + \
+    right_down_angle + Fore.RESET + Style.RESET_ALL
 
-#puzzle print function
+# puzzle print function
+
+
 def print_puzzle(array):
     print(first_line)
     for a in range(len(array)):
@@ -43,7 +51,9 @@ def print_puzzle(array):
         else:
             print(middle_line)
 
-#it is the node which store each state of puzzle
+# it is the node which store each state of puzzle
+
+
 class Node:
     def __init__(self, current_node, previous_node, g, h, dir):
         self.current_node = current_node
@@ -61,7 +71,9 @@ def get_pos(current_state, element):
         if element in current_state[row]:
             return (row, current_state[row].index(element))
 
-#it is a distance calculation algo
+# it is a distance calculation algo
+
+
 def euclidianCost(current_state):
     cost = 0
     for row in range(len(current_state)):
@@ -70,23 +82,30 @@ def euclidianCost(current_state):
             cost += abs(row - pos[0]) + abs(col - pos[1])
     return cost
 
-#get adjucent Nodes
+# get adjucent Nodes
+
+
 def getAdjNode(node):
     listNode = []
     emptyPos = get_pos(node.current_node, 0)
 
     for dir in DIRECTIONS.keys():
-        newPos = (emptyPos[0] + DIRECTIONS[dir][0], emptyPos[1] + DIRECTIONS[dir][1])
+        newPos = (emptyPos[0] + DIRECTIONS[dir][0],
+                  emptyPos[1] + DIRECTIONS[dir][1])
         if 0 <= newPos[0] < len(node.current_node) and 0 <= newPos[1] < len(node.current_node[0]):
             newState = deepcopy(node.current_node)
-            newState[emptyPos[0]][emptyPos[1]] = node.current_node[newPos[0]][newPos[1]]
+            newState[emptyPos[0]][emptyPos[1]
+                                  ] = node.current_node[newPos[0]][newPos[1]]
             newState[newPos[0]][newPos[1]] = 0
             # listNode += [Node(newState, node.current_node, node.g + 1, euclidianCost(newState), dir)]
-            listNode.append(Node(newState, node.current_node, node.g + 1, euclidianCost(newState), dir))
+            listNode.append(Node(newState, node.current_node,
+                            node.g + 1, euclidianCost(newState), dir))
 
     return listNode
 
-#get the best node available among nodes
+# get the best node available among nodes
+
+
 def getBestNode(openSet):
     firstIter = True
 
@@ -97,7 +116,9 @@ def getBestNode(openSet):
             bestF = bestNode.f()
     return bestNode
 
-#this functionn create the smallest path
+# this functionn create the smallest path
+
+
 def buildPath(closedSet):
     node = closedSet[str(END)]
     branch = list()
@@ -116,9 +137,12 @@ def buildPath(closedSet):
 
     return branch
 
-#main function of node
+# main function of node
+
+
 def main(puzzle):
-    open_set = {str(puzzle): Node(puzzle, puzzle, 0, euclidianCost(puzzle), "")}
+    open_set = {str(puzzle): Node(
+        puzzle, puzzle, 0, euclidianCost(puzzle), "")}
     closed_set = {}
 
     while True:
@@ -131,7 +155,7 @@ def main(puzzle):
         adj_node = getAdjNode(test_node)
         for node in adj_node:
             if str(node.current_node) in closed_set.keys() or str(node.current_node) in open_set.keys() and open_set[
-                str(node.current_node)].f() < node.f():
+                    str(node.current_node)].f() < node.f():
                 continue
             open_set[str(node.current_node)] = node
 
@@ -139,8 +163,11 @@ def main(puzzle):
 
 
 if __name__ == '__main__':
-    #it is start matrix
-   
+    # it is start matrix
+    END = [[1, 2, 3], [4, 5, 6], [7, 8, 0]]
+    br = main([[1, 2, 3],
+               [0, 4, 6],
+               [7, 5, 8]])
 
     print('total steps : ', len(br) - 1)
     print()
@@ -156,8 +183,10 @@ if __name__ == '__main__':
                 letter = 'LEFT'
             elif b['dir'] == 'D':
                 letter = 'DOWN'
-            print(dash + dash + right_junction, letter, left_junction + dash + dash)
+            print(dash + dash + right_junction,
+                  letter, left_junction + dash + dash)
         print_puzzle(b['node'])
         print()
 
-    print(dash + dash + right_junction, 'ABOVE IS THE OUTPUT', left_junction + dash + dash)
+    print(dash + dash + right_junction,
+          'ABOVE IS THE OUTPUT', left_junction + dash + dash)
